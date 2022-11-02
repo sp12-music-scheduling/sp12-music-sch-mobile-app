15a564fba0dda1def04803bc40d70aa671d8253f
tmoloi <tmoloi@students.kennesaw.edu>
2022-11-02 17:21:24 -0400

    Pulling latest project version

--- App.js
+++ App.js
@@ -3,43 +3,43 @@ import { NavigationContainer } from "@react-navigation/native";
 import DrawerNavigator from "./src/navigation/teacher/DrawerNavigator";
 import { getDBConnection, createTables, clearDatabase, getPracticeTypes, insertDefaultPracticeTypes, insertDefaultUserRoles, getUserRoles, getDemoTeacherUser } from "./src/services/database";
 
- const App = () => {
+//  const App = () => {
   
-  const [user, setUser] = useState('');
+//   const [user, setUser] = useState('');
 
-  const loadDataCallback = useCallback(async () => {
-    try {
-      const db = await getDBConnection();
-      // await clearDatabase(db);
-      await createTables(db);
-      const user_roles = await getUserRoles(db);
-      if (user_roles.length == 0) {
-        await insertDefaultUserRoles(db);
-      }
-      const practice_types = await getPracticeTypes(db);
-      if (practice_types.length == 0) {
-        await insertDefaultPracticeTypes(db);
-      }
-      // TMP CREATING USER
-      setUser(await getDemoTeacherUser(db));
-    } catch (error) {
-      console.error(error);
-    }
-  }, []);
+//   const loadDataCallback = useCallback(async () => {
+//     try {
+//       const db = await getDBConnection();
+//       // await clearDatabase(db);
+//       await createTables(db);
+//       const user_roles = await getUserRoles(db);
+//       if (user_roles.length == 0) {
+//         await insertDefaultUserRoles(db);
+//       }
+//       const practice_types = await getPracticeTypes(db);
+//       if (practice_types.length == 0) {
+//         await insertDefaultPracticeTypes(db);
+//       }
+//       // TMP CREATING USER
+//       setUser(await getDemoTeacherUser(db));
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }, []);
   
-  useEffect(() => {
-    loadDataCallback();
-  }, [loadDataCallback]);
+//   useEffect(() => {
+//     loadDataCallback();
+//   }, [loadDataCallback]);
 
-  console.log('APP <user>',user);
-  return (
-    <NavigationContainer>
-      <DrawerNavigator user={user} />
-    </NavigationContainer>
-  );
-}
+//   console.log('APP <user>',user);
+//   return (
+//     <NavigationContainer>
+//       <DrawerNavigator user={user} />
+//     </NavigationContainer>
+//   );
+// }
 
-export default App;
+// export default App;
 
 /**
  * 
@@ -49,25 +49,25 @@ export default App;
  */
 
 //  import React from 'react';
-//  import {SafeAreaView, StyleSheet, Text} from 'react-native';
-//  import Navigation from './src/navigation/login';
+ import {SafeAreaView, StyleSheet, Text} from 'react-native';
+ import Navigation from './src/navigation/login';
  
  
  
-//  const App = () => {
-//    return (
-//      <SafeAreaView style={styles.root}>
-//        <Navigation />
-//      </SafeAreaView>
-//    );
-//  };
+ const App = () => {
+   return (
+     <SafeAreaView style={styles.root}>
+       <Navigation />
+     </SafeAreaView>
+   );
+ };
  
-//  const styles = StyleSheet.create({
-//    root: {
-//      flex: 1,
-//      backgroundColor: '#F9FBFC'
-//    },
-//  });
+ const styles = StyleSheet.create({
+   root: {
+     flex: 1,
+     backgroundColor: '#F9FBFC'
+   },
+ });
  
-//  export default App;
+ export default App;
  
