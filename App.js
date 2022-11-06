01a2ec7f07cac34ff933f8d9ee2d9c8539b86b37
Alejandro <alejandrozic@gmail.com>
2022-11-06 09:34:02 -0500

    incremental changes

--- App.js
+++ App.js
@@ -1,71 +1,52 @@
-import React, {useState, useCallback, useEffect } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import DrawerNavigator from "./src/navigation/teacher/DrawerNavigator";
-import {
-  getDBConnection,
-  createTables,
-  clearDatabase,
-  getPracticeTypes,
-  insertDefaultPracticeTypes,
-  insertDemoStudentUsers,
-  getUsers,
- } from "./src/services/database";
+// import React, {useState, useCallback, useEffect } from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import DrawerNavigator from "./src/navigation/teacher/DrawerNavigator";
+// import {
+//   getDBConnection,
+//   createTables,
+//   clearDatabase,
+//   getPracticeTypes,
+//   insertDefaultPracticeTypes,
+//   insertDemoStudentUsers,
+//   getUsers,
+//  } from "./src/services/database";
 
-
- const App = () => {
-
-  const [user, setUser] = useState('');
 
 //  const App = () => {
-  
-//   const [user, setUser] = useState('');
+
+//   const loadDataCallback = useCallback(async () => {
+//     try {
+//       const db = await getDBConnection();
+//       // await clearDatabase(db);
+//       await createTables(db);
+
+//       /**
+//        * Default Pratice Types
+//        * Inject the default practice types of empty.
+//       */
+
+//       const practice_types = await getPracticeTypes(db);
+//       if (practice_types.length == 0) {
+//         await insertDefaultPracticeTypes(db);
+//       }
+//       // TMP CREATING USER
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }, []);
 
 
-  const loadDataCallback = useCallback(async () => {
-    try {
-      const db = await getDBConnection();
-      // await clearDatabase(db);
-      await createTables(db);
-
-      /**
-       * Default Pratice Types
-       * Inject the default practice types of empty.
-      */
-
-      const user_roles = await getUserRoles(db);
-      if (user_roles.length == 0) {
-        await insertDefaultUserRoles(db);
-      }
-
-      const practice_types = await getPracticeTypes(db);
-      if (practice_types.length == 0) {
-        await insertDefaultPracticeTypes(db);
-      }
-      // TMP CREATING USER
-      setUser(await getDemoTeacherUser(db));
-    } catch (error) {
-      console.error(error);
-    }
-  }, []);
-
-
-  useEffect(() => {
-    loadDataCallback();
-  }, [loadDataCallback]);
-
-  
 //   useEffect(() => {
 //     loadDataCallback();
 //   }, [loadDataCallback]);
 
 
-  console.log('APP <user>',user);
-  return (
-    <NavigationContainer>
-      <DrawerNavigator user={user} />
-    </NavigationContainer>
-  );
-}
+//   return (
+//     <NavigationContainer>
+//       <DrawerNavigator/>
+//     </NavigationContainer>
+//   );
+// }
 
 // export default App;
 
@@ -76,7 +57,7 @@ import {
  * 
  */
 
-//  import React from 'react';
+ import React from 'react';
  import {SafeAreaView, StyleSheet, Text} from 'react-native';
  import Navigation from './src/navigation/login';
  
@@ -98,7 +79,7 @@ import {
  });
  
 
-//  export default App;
+ export default App;
 
 /* Student View */
 
